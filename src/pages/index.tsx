eb6e8e5d9cd6a134f1c5d779b95e68c013e437e5
KieranProctorMVH <kieranp@mvhltd.com>
2022-08-03 10:45:20 +0100

    g

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
+        <h1 className="hero__title"><a href="https://github.com/KieranLProctor/kui">@kieranlproctor/kui</a></h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
@@ -30,8 +30,8 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Kui - React, Tailwind component library`}
+      description="Kui is a React component library built using Tailwind CSS for styling.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
